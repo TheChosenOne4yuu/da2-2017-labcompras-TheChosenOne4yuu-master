c71660319f13baa1f22d33ba286ae0c2fc6ac770
150323040 <150323040@ESCE-D216-05>
2017-03-24 17:59:12 +0000

    Add project files.

---
+++ src/DA2-2017-LABCOMPRAS/Models/Despesa.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DA2_2017_LABCOMPRAS.Models
+{
+    public class Despesa
+    {
+        public string NomeDespesa;
+        public int Quantidade;
+        public double Valor;
+        public string Categoria;
+        public bool Pago;
+
+        public string nomedespesas
+        {
+            get { return NomeDespesa; }
+            set { NomeDespesa = value; }
+        }
+
+        public int quantidade
+        {
+            get { return Quantidade; }
+            set { Quantidade = value; }
+
+        }
+
+        public double valor
+        {
+            get { return Valor; }
+            set { Valor = value; }
+        }
+
+        public string categoria
+        {
+            get { return Categoria; }
+            set { Categoria = value; }
+        }
+
+        public bool pago
+        {
+            get { return Pago; }
+            set { Pago = value; }
+        }
+        
+    }
+}
